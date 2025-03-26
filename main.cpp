cb53f205cf69fceb0aed18b25b0de2b70b4ec507
sergio mayen <smayen@bytesw.com>
2025-03-25 22:58:12 -0600

    25 / 03 : se agrega funcion primitiva de traduccion

--- main.cpp
+++ main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>  // Para manejar JSON
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+const std::string API_KEY = "TU_CLAVE_DE_API"; // Sustituye con tu clave real
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+std::string traducirTexto(const std::string& texto, const std::string& origen, const std::string& destino) {
+    std::string url = "https://translation.googleapis.com/language/translate/v2?key=" + API_KEY;
+
+    // Cuerpo de la solicitud en formato JSON
+    nlohmann::json requestBody = {
+        {"q", texto},
+        {"source", origen},
+        {"target", destino},
+        {"format", "text"}
+    };
+
+    // Enviar la solicitud POST
+    cpr::Response response = cpr::Post(
+        cpr::Url{url},
+        cpr::Header{{"Content-Type", "application/json"}},
+        cpr::Body{requestBody.dump()}
+    );
+
+    // Manejar la respuesta
+    if (response.status_code == 200) {
+        auto jsonResponse = nlohmann::json::parse(response.text);
+        return jsonResponse["data"]["translations"][0]["translatedText"];
+    } else {
+        return "Error en la traducción: " + response.text;
     }
-
-    return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+int main() {
+    std::string texto = "Hola, mundo";
+    std::string traduccion = traducirTexto(texto, "es", "en");
+    std::cout << "Traducción: " << traduccion << std::endl;
+    return 0;
+}
---
+++ services/translate.cpp
@@ -0,0 +1,20 @@
+//nlohmann::json requestBodyFrance = {
+//    {"q", texto},
+//    {"source", "auto"},
+//    {"target", "fr"},
+//    {"format", "text"}
+//};
+//
+//nlohmann::json requestBodyGerman = {
+//    {"q", texto},
+//    {"source", "auto"},
+//    {"target", "ger"},
+//    {"format", "text"}
+//};
+//
+//nlohmann::json requestBodyEn = {
+//    {"q", texto},
+//    {"source", "auto"},
+//    {"target", "en"},
+//    {"format", "text"}
+//};
