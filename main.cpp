3897673df46eba7419514677e971d3e3e3b426d1
sergio mayen <smayen@bytesw.com>
2025-03-29 00:29:08 -0600

    Se agregan idiomas de traduccion

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include <curl/curl.h>
 #include <string>
 #include <nlohmann/json.hpp>
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
 
 const string API_KEY = "AIzaSyC8ishIK-BIvWWHmgf5Gkt3rBKO39YEUYg";
-//const string API_KEY = "key";//
+struct Traducciones {
+    vector<string> targets = {"es","it", "fr", "de", "en"};
+};
 
-const string TARGET_LANG = "es";
 const string TEXT_TO_TRANSLATE = "Hello";
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output) {
@@ -24,36 +26,47 @@ string extractTranslation(const string& jsonResponse) {
 }
 
 string translateText(const string& text) {
+    Traducciones tr;
     CURL* curl;
     CURLcode res;
-    string response;
+    string translatedText = "";
 
     string url = "https://translation.googleapis.com/language/translate/v2?key=" + API_KEY;
-    string postData = "q=" + text + "&target=" + TARGET_LANG;
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_POST, 1);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    for (const auto& target : tr.targets) {
+        string response;
+        string postData = "q=" + text + "&target=" + target;
 
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            cerr << "cURL error: " << curl_easy_strerror(res) << endl;
+        curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_POST, 1);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+            res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                cerr << "cURL error: " << curl_easy_strerror(res) << endl;
+            } else {
+                translatedText += "Traducción a " + target + ": " + extractTranslation(response) + "\n";
+            }
+
+            curl_easy_cleanup(curl);
         }
-
-        curl_easy_cleanup(curl);
     }
-    return extractTranslation(response);
+
+    return translatedText;
 }
 
-
-
 int main() {
+    string textEntry;
 
-    string translatedText = translateText(TEXT_TO_TRANSLATE);
-    cout << "Respuesta de Google Translate API:\n" << translatedText << endl;
+    cout << "Ingrese el texto a traducir:\n";
+    getline(cin, textEntry);
+
+    string translatedText = translateText(textEntry);
+    cout << "Respuestas de Google Translate API:\n" << translatedText << endl;
     return 0;
-    }
+}
+
